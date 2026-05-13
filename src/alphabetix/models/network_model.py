172d786477a2fd759dfaba91e66111bb7a9850be
Alicia Lu <lualicia88@gmail.com>
2026-05-13 10:44:20 -0400

    Compute exc/inh activation and current source separately.

--- src/alphabetix/models/network_model.py
+++ src/alphabetix/models/network_model.py
@@ -1,68 +1,47 @@
-import equinox as eqx
 import jax
-import jax.numpy as jnp
 
 from ..module import Module
-from .neuron_model import NeuronModel
+from .neuron_model import NeuronModel, Constants
 
 
 class NetworkModel(Module):
-    neurons: NeuronModel
-
-    num_exc: int = eqx.field(static=True)
-    num_pv: int = eqx.field(static=True)
-    num_som: int = eqx.field(static=True)
-
     # rows: post-synaptic neurons
     # columns: pre-synaptic neurons
+    # connectivity: conductance weights, unit nS
     connectivity: jax.Array  # (num_neurons, num_neurons)
 
-    # LIF model constants
-    tau_m: jax.Array  # dependent on cell types
-
-    def __init__(
-        self,
-        neurons: NeuronModel,
-        num_exc: int,
-        num_pv: int,
-        num_som: int,
-    ):
-        self.neurons = neurons
-        self.num_exc = num_exc
-        self.num_pv = num_pv
-        self.num_som = num_som
-        tau_exc = 2 / 1000
-        tau_pv = 3.1 / 1000
-        tau_som = 11.8 / 1000
-        # self.tau_m: (num_neurons,)
-        # neuron ordering: Exc -> PV -> SOM
-        self.tau_m = jnp.concatenate(
-            [
-                jnp.full((self.num_exc,), tau_exc),
-                jnp.full((self.num_pv,), tau_pv),
-                jnp.full((self.num_som,), tau_som),
-            ]
+    def step(self, neurons, input_activations, dt):
+        activations, currents = self._compute_activations_and_currents(
+            neurons, input_activations, dt
         )
-
-    @property
-    def num_neurons(self):
-        return self.num_exc + self.num_pv + self.num_som
-
-    def step(self, network_activations, input_activations, dt):
-        # at t = 0, `network_activations` = `input_activations`
-        activations = self.compute_activations(
-            network_activations, input_activations, dt
+        next_neurons = jax.vmap(NeuronModel.update, in_axes=(0, 0, 0, None))(
+            neurons, activations, currents, dt
         )
-        next_neurons = jax.vmap(NeuronModel.update, in_axes=(0, 0, None))(
-            self.neurons, activations, dt
-        )
-        return self.replace(neurons=next_neurons)
+        return next_neurons
 
-    def compute_activations(self, network_activations, input_activations, dt):
+    def _compute_activations_and_currents(self, neurons, input_activations, dt):
+        # update activations according to connectivity
+        # TODO: tau should be synaptic decay time constant (neuron-type
+        # specific) not the generic membrane decay time constant
+        exc_presynaptic_neurons = neurons.sign > 0
+        inh_presynaptic_neurons = neurons.sign < 0
+
         network_activations = (
-            1 - dt / self.tau_m
-        ) * network_activations + self.connectivity @ (
-            self.neurons.spike * self.neurons.sign
+            1 - dt / neurons.tau_synapse
+        ) * neurons.activation + self.connectivity @ (neurons.spike * neurons.sign)
+
+        exc_connectivity = self.connectivity * exc_presynaptic_neurons[None, :]
+        inh_connectivity = self.connectivity * inh_presynaptic_neurons[None, :]
+
+        exc_current = (
+            exc_connectivity
+            @ (network_activations + input_activations)
+            * (neurons.voltage - Constants.exc_reversal_potential)
+        )
+        inh_current = (
+            inh_connectivity
+            @ network_activations
+            * (neurons.voltage - Constants.inh_reversal_potential)
         )
 
-        return network_activations + input_activations
+        return network_activations, exc_current + inh_current
