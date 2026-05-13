2d87225ee45749acb057515657aad0e13c0a38f7
Alicia Lu <lualicia88@gmail.com>
2026-05-13 12:03:08 -0400

    Update init files.

--- src/alphabetix/__init__.py
+++ src/alphabetix/__init__.py
@@ -1,1 +1,6 @@
 __version__ = "0.0.1"
+
+from .models import InputModel, NetworkModel, NeuronModel, Timeline
+from .simulate import run_simulation
+
+__all__ = ["InputModel", "NetworkModel", "NeuronModel", "Timeline", "run_simulation"]
--- src/alphabetix/models/__init__.py
+++ src/alphabetix/models/__init__.py
@@ -1,4 +1,6 @@
 from .input_model import InputModel
+from .network_model import NetworkModel
 from .neuron_model import NeuronModel
+from .timeline import Timeline
 
-__all__ = ["InputModel", "NeuronModel"]
+__all__ = ["InputModel", "NetworkModel", "NeuronModel", "Timeline"]
