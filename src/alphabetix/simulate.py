95ee31280f7a6af42689fc1dcf45406b98a3215b
Alicia Lu <lualicia88@gmail.com>
2026-05-13 11:59:33 -0400

    Update simulation.

--- src/alphabetix/simulate.py
+++ src/alphabetix/simulate.py
@@ -1,62 +1,31 @@
 import jax
+import jax.numpy as jnp
 
-from .model import InputModel, NeuronModel
-from .sensor import Sensor
-from .state import NeuronStates
+from .models import NetworkModel, NeuronModel
 
 
 def run_simulation(
-    neuron_model: NeuronModel,
-    sensor: Sensor,
-    key: jax.Array,
+    network: NetworkModel,
+    neurons: NeuronModel,
+    dt: float,
+    input_activations: jax.Array,  # precomputed from task
 ):
-    key, subkey = jax.random.split(key)
-    initial_neuron_states = neuron_model.initialize_neuron_states(subkey)
-    # NOTE: initialize timeline under neuron_model?
-
-    # generate external inputs from cortex and thalamus
-    key, subkey = jax.random.split(key)
-    input_model = InputModel(neuron_model, subkey)
-
-    input_activations = input_model.compute_activations()
-    # input_activations: (t, n), bool
-    # t = timesteps
-    # n = number of internal neurons
-
-    def scan_fn(neuron_state, input_activation):
-        neuron_state, readout = simulation_step(
-            neuron_model, neuron_state, input_activation, sensor
+    def scan_fn(neurons, input_activations):
+        neurons = network.step(
+            neurons,
+            input_activations,
+            dt,
         )
-        return neuron_state, (neuron_state, readout)
+        carry = neurons
+        next_state = neurons
+        return carry, next_state
 
-    _, (neuron_states, readouts) = jax.lax.scan(
-        scan_fn, initial_neuron_states, input_activations
+    init_carry = neurons
+    _, neurons = jax.lax.scan(scan_fn, init_carry, input_activations)
+
+    # add initial neurons to all other neurons returned from scan
+    neurons = jax.tree.map(
+        lambda a, i: jnp.insert(a, 0, i, axis=0), neurons, init_carry
     )
 
-    return neuron_states, readouts
-
-
-def simulation_step(
-    neuron_model: NeuronModel,
-    neuron_state: NeuronStates,
-    input_activations: jax.Array,
-    sensor: Sensor,
-) -> tuple[NeuronStates, jax.Array]:
-    """Update the internal neuron states and produce sensor readings."""
-    # 1. compute internal activations (n,)
-    internal_activations = neuron_model.compute_activations(neuron_state)
-
-    # 2. add to input activations (n,)
-    total_activations = internal_activations + input_activations
-
-    # 3. compute next currents and voltages
-    currents = neuron_model.update_currents(neuron_state, total_activations)
-    voltages = neuron_model.update_voltages(neuron_state, currents)
-
-    # 4. update the neuron state
-    neuron_state = neuron_state.replace(currents=currents, voltages=voltages)
-
-    # 5. generate measurements
-    readout = sensor.measure(neuron_state)
-
-    return neuron_state, readout
+    return neurons
