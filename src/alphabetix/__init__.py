c7c40a61890608bbeb03b5046c479a9e5809ff79
Alicia Lu <lualicia88@gmail.com>
2026-07-07 11:01:35 -0400

    Expose states and probes

--- src/alphabetix/__init__.py
+++ src/alphabetix/__init__.py
@@ -3,19 +3,32 @@ __version__ = "0.0.1"
 from .models import (
     DecoderModel,
     InputModel,
+    Model,
+    Network,
     NetworkModel,
+    Neuron,
     NeuronModel,
     Timeline,
 )
-from .simulate import run_simulation
+
 from .electrodes import Electrode
+from .record import ActivationProbe, CurrentProbe, VoltageProbe, SpikeProbe, Probes
+from .simulate import run_simulation
 
 __all__ = [
+    "ActivationProbe",
+    "CurrentProbe",
     "DecoderModel",
     "Electrode",
     "InputModel",
+    "Model",
+    "Network",
     "NetworkModel",
+    "Neuron",
     "NeuronModel",
+    "Probes",
+    "SpikeProbe",
     "Timeline",
+    "VoltageProbe",
     "run_simulation",
 ]
--- src/alphabetix/models/__init__.py
+++ src/alphabetix/models/__init__.py
@@ -1,13 +1,19 @@
 from .decoder_model import DecoderModel
 from .input_model import InputModel
+from .model import Model
+from .network import Network
 from .network_model import NetworkModel
+from .neuron import Neuron
 from .neuron_model import NeuronModel
 from .timeline import Timeline
 
 __all__ = [
     "DecoderModel",
     "InputModel",
+    "Model",
+    "Network",
     "NetworkModel",
+    "Neuron",
     "NeuronModel",
     "Timeline",
 ]
