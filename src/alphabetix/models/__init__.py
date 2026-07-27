a036544dc2e3ba547cfbaa0b8e8ea629568fcedf
Alicia Lu <lualicia88@gmail.com>
2026-07-26 21:37:18 -0400

    Expose new models

--- src/alphabetix/models/__init__.py
+++ src/alphabetix/models/__init__.py
@@ -1,5 +1,6 @@
+from .adaptive_lif_neuron_model import AdaptiveNeuronModel
 from .decoder_model import DecoderModel
-from .input_model import InputModel
+from .input_model import ExplicitInputModel, InputModel
 from .model import Model
 from .network import Network
 from .network_model import NetworkModel
@@ -8,12 +9,15 @@ from .neuron_model import NeuronModel
 from .timeline import Timeline
 
 __all__ = [
+    "AdaptiveNeuronModel",
     "DecoderModel",
+    "ExplicitInputModel",
     "InputModel",
     "Model",
     "Network",
     "NetworkModel",
     "Neuron",
+    "NeuronModel",
     "NeuronModel",
     "Timeline",
 ]
