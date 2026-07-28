6e417ddb8e965da48262ae21bbf9159b3c2eb0fc
Alicia Lu <lualicia88@gmail.com>
2026-07-28 12:33:49 -0400

    Make InputModel abstract class
    Current InputModel -> TimelineInputModel

--- src/alphabetix/models/input_model.py
+++ src/alphabetix/models/input_model.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod
+
 import jax
 import jax.numpy as jnp
 
@@ -7,7 +9,51 @@ from .timeline import Timeline
 from .topdown_model import TopDownModel
 
 
+@abstractmethod
 class InputModel(Module):
+    """Abstract base class for all input-current generators."""
+
+    @abstractmethod
+    def compute_currents(self, dt: float) -> jax.Array:
+        """Return input currents with shape (num_timesteps, num_neurons)."""
+        raise NotImplementedError
+
+
+class ExplicitInputModel(InputModel):
+    currents: jax.Array = Module.param()
+
+    def compute_currents(self, dt: float):
+        return self.currents
+
+
+class TonicInputModel(InputModel):
+    exc_bias: jax.Array = Module.param()
+    inh_bias: jax.Array = Module.param()
+    cue_amplitude: jax.Array = Module.param()
+
+    num_exc: int = Module.static()
+    num_inh: int = Module.static()
+    num_timesteps: int = Module.static()
+    cue_steps: int = Module.static()
+
+    def compute_currents(self, dt):
+        exc = jnp.full(
+            (self.num_timesteps, self.num_exc),
+            self.exc_bias,
+        )
+        inh = jnp.full(
+            (self.num_timesteps, self.num_inh),
+            self.inh_bias,
+        )
+
+        inputs = jnp.concatenate([exc, inh], axis=1)
+
+        cue = jnp.arange(self.num_timesteps) < self.cue_steps
+        inputs = inputs.at[:, : self.num_exc].add(cue[:, None] * self.cue_amplitude)
+        return inputs
+
+
+class TimelineInputModel(InputModel):
     # component models
     sensory_model: SensoryModel
     topdown_model: TopDownModel
