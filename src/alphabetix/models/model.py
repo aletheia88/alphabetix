2a0a106cc715c72a5ff73da6af391d106b670515
Alicia Lu <lualicia88@gmail.com>
2026-08-14 14:53:27 -0400

    Add decoder model to `Model` class

--- src/alphabetix/models/model.py
+++ src/alphabetix/models/model.py
@@ -1,10 +1,12 @@
 from ..module import Module
+from .decoder_model import DecoderModel
 from .input_model import InputModel
 from .network_model import NetworkModel
 from .neuron_model import NeuronModel
 
 
 class Model(Module):
+    decoder_model: DecoderModel
     input_model: InputModel
     network_model: NetworkModel
     neuron_model: NeuronModel
@@ -12,11 +14,13 @@ class Model(Module):
 
     def __init__(
         self,
+        decoder_model: DecoderModel,
         input_model: InputModel,
         network_model: NetworkModel,
         neuron_model: NeuronModel,
         dt: float,
     ):
+        self.decoder_model = decoder_model
         self.input_model = input_model
         self.network_model = network_model
         self.neuron_model = neuron_model
