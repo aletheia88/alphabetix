541655874e558f0910d29814d2783b9439fda72a
Alicia Lu <lualicia88@gmail.com>
2026-08-23 11:16:33 -0400

    Update trainable network state parameters

--- src/alphabetix/models/network_model.py
+++ src/alphabetix/models/network_model.py
@@ -6,8 +6,8 @@ from ..module import Module
 
 class NetworkModel(Module):
     # parameters for background currents
-    mean_bg_current: jnp.float32 = Module.static()
-    sigma_bg: jnp.float32 = Module.static()
+    mean_bg_current: jnp.float32 = Module.param()
+    sigma_bg: jnp.float32 = Module.param()
     tau_bg: jnp.float32 = Module.static()
 
     # connectivity: conductance weights, unit nS
